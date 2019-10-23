ba559a041399de207b0cda768210d019f55fa7fe
chenqixian <chenqixian@buaa.edu.cn>
2019-10-23 10:33:53 +0800

    remove

--- remove.js
+++ remove.js
@@ -5,8 +5,8 @@
  */
 function remove(arr, item) {
     let res = [];
-    for(let i = 0, len = arr.length; i < len ; i ++) {
-        if(arr[i] !== item) {
+    for (let i = 0, len = arr.length; i < len; i++) {
+        if (arr[i] !== item) {
             res.push(arr[i]);
         }
     }
@@ -14,7 +14,22 @@ function remove(arr, item) {
 }
 
 function remove(arr, item) {
-    return arr.filter(function(cur) {
+    return arr.filter(function (cur) {
         return cur !== item;
     })
+}
+
+/**
+ * 移除数组 arr 中的所有值与 item 相等的元素。直接修改数组 arr，结果返回新的数组
+ * @param {Array} arr 
+ * @param {Number} item 
+ */
+function removeWithoutCopy(arr, item) {
+    for(let i = 0 ; i < arr.length ; i ++) {
+        if(arr[i] === item) {
+            arr.splice(i,1);
+            i --;
+        }
+    }
+    return arr;
 }
