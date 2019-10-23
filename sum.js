60c802434f15739ecb1513bc26dd8bc8a9eac113
chenqixian <chenqixian@buaa.edu.cn>
2019-10-23 10:15:39 +0800

    sum

--- sum.js
+++ sum.js
@@ -1,5 +1,17 @@
+/**
+ * 求和
+ * @param {Array} arr 
+ */
 function sum(arr) {
     return arr.reduce(function(prev, next) {
         return prev + next;
     })
+}
+
+function sum(arr) {
+    let res = 0;
+    arr.forEach(element => {
+        res += element; 
+    });
+    return res;
 }
