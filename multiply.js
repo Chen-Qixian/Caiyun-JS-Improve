135d2fdbf8e529ee97eaa14af19f3c318b306b85
chenqixian <chenqixian@buaa.edu.cn>
2019-10-31 11:45:22 +0800

    multiply

--- multiply.js
+++ multiply.js
@@ -1,3 +1,8 @@
+/**
+ * 求 a 和 b 相乘的值，a 和 b 可能是小数，需要注意结果的精度问题
+ * @param {Number} a 
+ * @param {Number} b 
+ */
 function multiply(a, b) {
     let stra = a.toString().split('.')[1] || '';
     let strb = b.toString().split('.')[1] || '';
